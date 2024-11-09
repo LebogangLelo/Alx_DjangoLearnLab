e4b92928ebaece1bf8935c4659d0e699b4d74535
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-11-09 05:15:38 +0200

    advanced models relationship

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,6 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # 1. Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
     return author.books.all()
 
 # 2. List all books in a library
