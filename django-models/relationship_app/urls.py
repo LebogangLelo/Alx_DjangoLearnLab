e0a285a725763ad7ad6bede521b64b410fcb6dad
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-11-10 00:35:39 +0200

    User authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LoginView, LogoutView, register
+from .views import list_books, LibraryDetailView, LoginView, LogoutView, register, views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
 
 
+
+                                                
