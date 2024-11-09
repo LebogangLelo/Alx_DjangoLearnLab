d400edef638d47ba0eea821f39da3da918c29522
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-11-09 23:22:25 +0200

    Django views and url configurations

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
+
 
 def list_books(request):
   books = Book.objects.all()
