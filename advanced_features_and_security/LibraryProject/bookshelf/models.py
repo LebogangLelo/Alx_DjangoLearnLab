2bf18d241f06d9ce6744920d98c074b750745740
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-11-16 12:22:33 +0200

    Custon user model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
 
-
-# Create your models here.
 class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=100)
@@ -11,3 +11,41 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
 
+
+class CustomUserManager(BaseUserManager):         # Custom manager for CustomUser.
+    
+      # Create and return a regular user with email and password.
+    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # Create and return a superuser with email and password.
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
+
+class CustomUser(AbstractUser):               #Custom user model extending AbstractUser.
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'date_of_birth']
+
+    def __str__(self):
+        return self.username
+    
+
+class SomeModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
