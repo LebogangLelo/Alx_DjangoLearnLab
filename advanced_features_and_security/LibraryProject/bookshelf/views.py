a02fee34aaa7c7c4986af4fb19ab1b647905295f
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-11-16 13:19:11 +0200

    Managing groups and permissions

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/edit_book.html', {'form': form})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    ...
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    ...
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    ...
+
+
