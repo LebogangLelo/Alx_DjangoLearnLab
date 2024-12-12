62ef045256d24ff3ea3937902c9d96b0628b82ef
Mahlatse-git <mahlatsemoses@gmail.com>
2024-12-12 22:12:03 +0200

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'bio', 'profile_picture', 'followers')
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'password', 'email', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+         
